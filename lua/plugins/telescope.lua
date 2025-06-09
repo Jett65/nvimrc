fb249040d30256eee318ff77c33fc2f58d3dc34c
Jett65 <jettc505@gmail.com>
2025-06-08 22:41:59 -0600

    reworked config

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,31 @@
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "debugloop/telescope-undo.nvim",
+        },
+
+		config = function()
+
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+			vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+			vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+			vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+            vim.keymap.set("n", "<leader>uu", "<cmd>Telescope undo<cr>")
+
+			require("telescope").setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<C-s>"] = "select_vertical"
+					  }
+					}
+				}
+			})	
+            require("telescope").load_extension("undo")
+		end
+	},
+}
