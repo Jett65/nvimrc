d654b330df911743d4a1a462d649bdb28968c0e2
Jett65 <jettc505@gmail.com>
2025-06-09 21:48:14 -0600

    got lua lsp and c lsp set up

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -28,4 +28,21 @@ return {
             require("telescope").load_extension("undo")
 		end
 	},
+
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+
+        config = function()
+            -- This is your opts table
+            require("telescope").setup {
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {
+                        }
+                    }
+                }
+            }
+            require("telescope").load_extension("ui-select")
+        end
+    },
 }
